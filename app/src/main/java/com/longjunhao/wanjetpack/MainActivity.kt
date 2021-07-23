e02bddd6997dd0f9e4f3c452c8ce2aa12ed7d21f
lelelongwang <1339024668@qq.com>
2021-07-23 18:33:11 +0800

    代码优化：沉侵式代码格式化

--- app/src/main/java/com/longjunhao/wanjetpack/MainActivity.kt
+++ app/src/main/java/com/longjunhao/wanjetpack/MainActivity.kt
@@ -9,15 +9,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
+import com.google.android.material.snackbar.Snackbar
 import com.longjunhao.wanjetpack.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navController = Navigation.findNavController(this, R.id.nav_host)
         binding.bottomNavigation.setupWithNavController(navController)
@@ -37,7 +40,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        setStatusBarAndNavigationBar()
+    }
 
+    private fun setStatusBarAndNavigationBar() {
         //Kotlin反射：去掉沉侵式后状态栏显示灰色阴影的问题。
         /*try {
             val decorView = window.decorView::class.java
@@ -53,19 +59,30 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
         }*/
 
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O_MR1) {
-            //以下代码实现了Android8.1上导航栏和状态栏的沉侵式，但是要结合themes中的设置
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-            //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS )
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-        }else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
-            //以下代码实现了Android10上导航栏的沉侵式，在themes中已经设置状态栏的沉侵式
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        // todo 后期可以把沉侵式统一在themes中设置，或者统一在代码中设置
+        when {
+            Build.VERSION.SDK_INT == Build.VERSION_CODES.O_MR1 -> {
+                //以下代码实现了Android8.1上导航栏和状态栏的沉侵式，但是要结合themes中的设置
+                window.clearFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
+                            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+                )
+                //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS )
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+                //以下代码实现了Android10上导航栏的沉侵式，在themes中已经设置状态栏的沉侵式
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+            else -> Snackbar.make(
+                binding.root,
+                "VERSION_CODES=${Build.VERSION.SDK_INT} 沉侵式 待适配",
+                Snackbar.LENGTH_LONG
+            ).show()
         }
     }
 
