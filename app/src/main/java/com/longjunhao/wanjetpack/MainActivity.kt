43bd2fce20c98b9d10731b068790d93ff1b1fec0
lelelongwang <1339024668@qq.com>
2021-08-07 17:32:53 +0800

    功能实现：Android11 上沉浸式适配。及优化Android10上登录界面的沉浸式。

--- app/src/main/java/com/longjunhao/wanjetpack/MainActivity.kt
+++ app/src/main/java/com/longjunhao/wanjetpack/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.longjunhao.wanjetpack
 
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -75,11 +77,42 @@ class MainActivity : AppCompatActivity() {
                         View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
                         View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
             }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+            Build.VERSION.SDK_INT == Build.VERSION_CODES.Q -> {
                 //以下代码实现了Android10上导航栏的沉侵式，在themes中已经设置状态栏的沉侵式
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                val uiMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                if (uiMode == Configuration.UI_MODE_NIGHT_YES) {
+                    //深色主题下，StatusBar不需要设置 View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR，亮色主题必须设置。
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                }else{
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
+                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                }
+            }
+            Build.VERSION.SDK_INT == Build.VERSION_CODES.R -> {
+                //Android11 沉浸式：
+                val uiMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                if (uiMode == Configuration.UI_MODE_NIGHT_YES) {
+                    // 深色主题下，Status的Light需要单独配置。
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                                View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                    window?.insetsController?.setSystemBarsAppearance(
+                        0,
+                        APPEARANCE_LIGHT_STATUS_BARS
+                    )
+                } else {
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or
+                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                    window?.insetsController?.setSystemBarsAppearance(
+                        APPEARANCE_LIGHT_STATUS_BARS,
+                        APPEARANCE_LIGHT_STATUS_BARS
+                    )
+                }
             }
             else -> Snackbar.make(
                 binding.root,
